cee1752e751d55ee045817a13fdae6177d05f86e
VESNA <vesna.acic23@gmail.com>
2023-02-05 21:35:47 +0100

    Napravljen Laravel projekat.

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       /*  $this->call(Proizvodjaci::class);
+        $this->call(Polovi::class);
+        $this->call(Artikli::class);
+        $this->call(Users::class); */
+    }
+}
